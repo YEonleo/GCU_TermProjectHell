3edbae56fff0ed554cec9ef7d510e1bb2b81d31d
YEonleo <90837906+YEonleo@users.noreply.github.com>
2022-11-12 00:49:48 +0900

    Update All.sh

--- All.sh
+++ All.sh
@@ -1,11 +1,11 @@
 #T5 모델 부터 inference
 
 cd T5
-# echo T5Model v1
-# bash v1.sh \
+echo T5Model v1
+bash v1.sh \
 
-# echo T5Model v2
-# bash v2.sh \
+echo T5Model v2
+bash v2.sh \
 
 cd ../kyElectra
 echo KyElectra
